8f6ab8498e5483b0eea2f8e55d80fa4bbb7fa222
iseki <admin@iseki.space>
2021-06-27 02:07:46 +0800

    add: persistent interface

---
+++ src/todo/IPersistent.ts
@@ -0,0 +1,27 @@
+import {Category, TodoItem, TodoItemState} from "./types";
+
+
+export interface CategoryPersistent {
+    onChange(handler: () => void): () => void
+
+    list(): Promise<Category[]>
+
+    put(category: Category): Promise<void>
+
+    update(category: Category): Promise<void>
+
+    remove(id: string): Promise<void>
+}
+
+export interface TodoPersistent {
+    onChange(handler: () => void): () => void
+
+    list({categoryId, status}: { categoryId?: string, status?: TodoItemState[], }): Promise<TodoItem[]>
+
+    put(todo: TodoItem): Promise<void>
+
+    update(todo: TodoItem): Promise<void>
+
+    remove(id: string): Promise<void>
+}
+
