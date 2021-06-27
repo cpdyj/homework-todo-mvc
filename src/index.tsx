59e7fc649d011a642f04cf06d5ee3fc0bc761cc4
iseki <admin@iseki.space>
2021-06-28 02:00:29 +0800

    (xD

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import("./87679624_p0.jpg").then(({default: miku}) => {
         console.log(miku)
         const img = new Image();
         img.src = miku;
-        console.log(miku)
         img.onload = () => {
             console.log("%c ", `background: no-repeat url("${miku}");padding:${img.height * .5 * .25}px ${img.width * .5 * .25}px;background-size: 100%;`)
         }
