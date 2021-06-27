faa15bd885fb054409387dc74d36abcd2b3a6ff0
iseki <admin@iseki.space>
2021-06-28 02:14:46 +0800

    呐~

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom';
 import {TodoApp} from "./todo/TodoApp";
 
 import("./87679624_p0.jpg").then(({default: miku}) => {
-    (() => {
-        console.log(miku)
-        const img = new Image();
-        img.src = miku;
-        img.onload = () => {
-            console.log("%c ", `background: no-repeat url("${miku}");padding:${img.height * .5 * .25}px ${img.width * .5 * .25}px;background-size: 100%;`)
-        }
-    })()
+    console.log("%c ", `background: no-repeat url("${miku}");padding:${1200 * .5 * .5}px ${765 * .5 * .5}px;background-size: 100%;`)
 });
 
 ReactDOM.render(<React.StrictMode>
